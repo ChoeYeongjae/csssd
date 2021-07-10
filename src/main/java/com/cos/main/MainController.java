767639dce3177592480032d491c350461de2f1ea
CYJ <CYJ@DESKTOP-IVPQUM2>
2021-07-11 01:43:45 +0900

    asdad

--- src/main/java/com/cos/main/MainController.java
+++ src/main/java/com/cos/main/MainController.java
@@ -27,7 +27,7 @@ public class MainController {
 	 * 메인 컨틀로러
 	 *    
 	 * */
-	@RequestMapping(value="/main")	
+	@RequestMapping("/")	
 	public String main() throws Exception{
 		return "index";
 	}
